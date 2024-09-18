bd4a7177fa85fd4b09ce8cb2ef1aa44134a2e3c0
Vulcan445 <165273424+Vulcan445@users.noreply.github.com>
2024-09-18 17:58:58 +0100

    2024-09-18 17:58:50

--- Problem1.py
+++ Problem1.py
@@ -4,9 +4,10 @@ from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime
 
 def outliers(data):
+    
     # Compute quartiles
     Q1 = np.percentile(data, 25)
-    Q2 = np.median(data)  # or np.percentile(data, 50)
+    # Q2 = np.median(data)  # or np.percentile(data, 50)
     Q3 = np.percentile(data, 75)
 
     # Compute interquartile range (IQR)
@@ -17,7 +18,8 @@ def outliers(data):
     upper_whisker = np.max(data[data <= Q3 + 1.5 * IQR])
 
     # Find outliers
-    outliers = data[(data < Q1 - 1.5 * IQR) | (data > Q3 + 1.5 * IQR)]
+    #outliers = np.where(data[(data < Q1 - 1.5 * IQR) | (data > Q3 + 1.5 * IQR)])
+    outliers = np.where((data < lower_whisker) | (data > upper_whisker))[0]
     
     return outliers
 
@@ -43,12 +45,11 @@ def linear_regression(X, y):
     Performs linear regression on the input data X and target y.
     """
     # Linear regression formula: (X^T X)^(-1) X^T y
-    beta = np.dot( np.dot(np.linalg.inv( np.dot(np.transpose(X),X) ), np.transpose(X)), y)  # (XT * X)** -1 * XT * Y
+    beta = np.matmul( np.matmul(np.linalg.inv( np.matmul(np.transpose(X),X) ), np.transpose(X)), y)  # (XT * X)** -1 * XT * Y
 
     beta = beta.flatten()  # Converts beta to shape (5,)
     
     return beta
-    
 
 def ridge_regression(X, y, lambda_param):
     """
@@ -62,7 +63,7 @@ def ridge_regression(X, y, lambda_param):
     
     # Ridge regression formula: (X^T X + λI)^(-1) X^T y
     ridge_term = lambda_param * I
-    beta = np.dot( np.dot(np.linalg.inv( np.add( np.dot(np.transpose(X),X), ridge_term) ), np.transpose(X)), y)
+    beta = np.matmul( np.matmul(np.linalg.inv( np.add( np.matmul(np.transpose(X),X), ridge_term) ), np.transpose(X)), y)
     
     beta = beta.flatten()  # Converts beta to shape (5,)
     
@@ -79,36 +80,42 @@ def main():
     Y = np.zeros((200,1))
     Y_rigid = np.zeros((200,1))
     y = y.reshape(-1, 1)
-    lambda_param = 0.2
-    
+    lambda_param = 100
+     
     # Normalize the entire dataset (all 5 features)
     
     normalized_data = scaler.fit_transform(data)
     y_normalised = scaler.fit_transform(y)
-    X = data[:, :5]  # Extracts the first 5 columns from data as a 2D array
-
+    X = normalized_data[:, :5]  # Extracts the first 5 columns from data as a 2D array
+    
     beta = linear_regression(X, y_normalised)
     
     beta_ridge = ridge_regression(X, y_normalised, lambda_param)
 
     for j in range(X.shape[0]):
-    # Calculate y_hat^j
-        Y[j] = np.dot(beta, X[j, :])
+    # Calculate y_pred
+        Y[j] = np.matmul(beta, X[j, :])
         
     for j in range(X.shape[0]):
-    # Calculate y_hat^j
-        Y_rigid[j] = np.dot(beta_ridge, X[j, :])
-
-    Y_normalised = scaler.fit_transform(Y)
+    # Calculate y_pred
+        Y_rigid[j] = np.matmul(beta_ridge, X[j, :])
+        
+    # Remove outliers from the data
     
-    Y_rigid_normalised = scaler.fit_transform(Y_rigid)
+    outliers_y = outliers(y_normalised)
 
-    r2 = Coefficient_of_Determination(y_normalised,Y_normalised)
+    y_normalised = np.delete(y_normalised,outliers_y)
+        
+    Y = np.delete(Y,outliers_y)    
+        
+    Y_rigid = np.delete(Y_rigid,outliers_y)
+
+    r2 = Coefficient_of_Determination(y_normalised,Y)
     
-    #r2_rigid = Coefficient_of_Determination(y_normalised,Y_rigid_normalised)
+    r2_rigid = Coefficient_of_Determination(y_normalised,Y_rigid)
     
     print("r2 :", r2)
-    #print("r2_rigid :", r2_rigid)
+    print("r2_rigid :", r2_rigid)
 
     # Get the current date and time
     current_time = datetime.now()
@@ -120,15 +127,15 @@ def main():
     
     plt.figure(figsize=(10, 6))
 
-    # plt.scatter(range(len(feature_1)), feature_1, color='red', label='Feature 1')
-    # plt.scatter(range(len(feature_2)), feature_2, color='blue', label='Feature 2')
-    # plt.scatter(range(len(feature_3)), feature_3, color='green', label='Feature 3')
-    # plt.scatter(range(len(feature_4)), feature_4, color='orange', label='Feature 4')
-    # plt.scatter(range(len(feature_5)), feature_5, color='purple', label='Feature 5')
+    # # plt.scatter(range(len(feature_1)), feature_1, color='red', label='Feature 1')
+    # # plt.scatter(range(len(feature_2)), feature_2, color='blue', label='Feature 2')
+    # # plt.scatter(range(len(feature_3)), feature_3, color='green', label='Feature 3')
+    # # plt.scatter(range(len(feature_4)), feature_4, color='orange', label='Feature 4')
+    # # plt.scatter(range(len(feature_5)), feature_5, color='purple', label='Feature 5')
     
     plt.scatter(range(len(y_normalised)), y_normalised, color='red', label='y')
-    plt.scatter(range(len(Y_normalised)), Y_normalised, color='blue', label='ypred')
-    #plt.scatter(range(len(Y_rigid_normalised)), Y_normalised, color='green', label='yrigid')
+    plt.scatter(range(len(Y)), Y, color='blue', label='ypred')
+    plt.scatter(range(len(Y_rigid)), Y_rigid, color='green', label='yrigid', s = 10)
 
     plt.title('Normalized Features')
     plt.xlabel('Index')
