7628c819c2d8ab4d4294a97505461a904965aeab
Vulcan445 <165273424+Vulcan445@users.noreply.github.com>
2024-10-01 11:26:19 +0100

    ...

--- 4.2/ARX.py
+++ 4.2/ARX.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import itertools
 
 def sse(y_true, y_pred):
         
